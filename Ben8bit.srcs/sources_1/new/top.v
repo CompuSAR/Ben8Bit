7aab22d62f010758037d8d9866fbef7278cadc86
Shachar Shemesh <shachar@driveu.auto>
2021-09-11 19:30:59 +0300

    Register B, bus

--- Ben8bit.srcs/sources_1/new/top.v
+++ Ben8bit.srcs/sources_1/new/top.v
@@ -21,17 +21,60 @@
 
 
 module top(
-    output [7:0] out
+    output [7:0] out,
+    input clock,
+    input bReset
     );
-    
+
+localparam BusIn_None = 0;
+localparam BusIn_RegA = 1;
+localparam BusIn_RegB = 2;
+localparam BusIn_ALU = 3;
+localparam BusIn_PC = 4;
+localparam BusIn_MemoryAddress = 5;
+localparam BusIn_Memory = 6;
+localparam BusIn_NumOptions = 7;
+
 wire [7:0]bus;
+reg [$clog2(BusIn_NumOptions-1)-1:0] bus_selector;
 
-assign bus = bus_data();
+wire [7:0]bus_inputs[BusIn_NumOptions-1:0];
+assign bus = bus_inputs[bus_selector];
 
-function bus_data();
+assign bus_inputs[BusIn_None] = 0;
+
+reg register_a_write;
+register reg_a(
+    .data_in(bus),
+    .data_out(bus_inputs[BusIn_RegA]),
+    .clock(clock),
+    .write_enable(register_a_write),
+    .bReset(bReset)
+);
+reg register_b_write;
+register reg_b(
+    .data_in(bus),
+    .data_out(bus_inputs[BusIn_RegB]),
+    .clock(clock),
+    .write_enable(register_b_write),
+    .bReset(bReset)
+);
+
+initial
+    reset_everything();
+
+task reset_everything();
 begin
-    bus_data = 0;
+    reset_controls();
 end
-endfunction
+endtask
+
+task reset_controls();
+begin
+    bus_selector <= BusIn_None;
+    register_a_write = 0;
+    register_b_write = 0;
+end
+endtask
 
 endmodule
